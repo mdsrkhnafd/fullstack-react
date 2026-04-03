520a62f36245e479ac4b89ecedb99eb5371cbbb3
Mudasir Khan <72373676+mdsrkhnafd@users.noreply.github.com>
2026-04-03 11:59:55 +0500

    updated

--- src/pages/404/404.jsx
+++ src/pages/404/404.jsx
@@ -1,8 +1,26 @@
+import "./404.css";
+
 export default function Error404() {
   return (
-    <>
-      <h1 className="text-4xl text-black font-bold">404- Page Not Found</h1>
-      <p>The page you are looking for does not exist</p>
-    </>
+    <div className="error404-page">
+      <div className="main">
+        <div className="card">
+          <h1>Page not found</h1>
+          <p>
+            Looks like you've followed a broken link or entered a URL that
+            doesn't exist on this site.
+          </p>
+          <hr />
+          <p className="your-site">
+            If this is your site, and you weren't expecting a 404 for this path,
+            please visit Netlify's{" "}
+            <a href="https://answers.netlify.com/t/support-guide-i-ve-deployed-my-site-but-i-still-see-page-not-found/125?utm_source=404page&utm_campaign=community_tracking">
+              &ldquo;page not found&rdquo; support guide
+            </a>{" "}
+            for troubleshooting tips.
+          </p>
+        </div>
+      </div>
+    </div>
   );
 }
